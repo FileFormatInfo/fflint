086d6e83fa1aa76c12ef8311f1d2f517ec342781
Andrew Marcuse <fileformat@gmail.com>
2022-05-21 10:36:44 -0400

    ADD: ico image size checks

--- internal/command/ico.go
+++ internal/command/ico.go
@@ -2,25 +2,48 @@ package command
 
 import (
 	"bytes"
+	"fmt"
+	"os"
 
 	"github.com/fileformat/badger/internal/shared"
 	"github.com/spf13/cobra"
 	"github.com/zyxar/image2ascii/ico"
 )
 
+var (
+	icoStrict    bool
+	icoStrictSet map[uint]bool
+	icoRequired  []uint
+	icoOptional  []uint
+)
+
 // icoCmd represents the ico command
 var icoCmd = &cobra.Command{
-	Args:  cobra.MinimumNArgs(1),
-	Use:   "ico [options] files...",
-	Short: "Validate icons",
-	Long:  `Validate that your ico files are valid`,
-	RunE:  shared.MakeFileCommand(icoCheck),
+	Args:    cobra.MinimumNArgs(1),
+	Use:     "ico [options] files...",
+	Short:   "Validate icons",
+	Long:    `Validate that your ico files are valid`,
+	PreRunE: icoPrepare,
+	RunE:    shared.MakeFileCommand(icoCheck),
 }
 
 func AddIcoCommand(rootCmd *cobra.Command) {
 	rootCmd.AddCommand(icoCmd)
 
-	//LATER: sizes flag (array of ints)
+	icoCmd.Flags().UintSliceVar(&icoRequired, "required", []uint{16, 32}, "Required sizes")
+	icoCmd.Flags().UintSliceVar(&icoOptional, "optional", []uint{48, 64, 96, 128}, "Optional sizes (only for `--strict`)")
+	icoCmd.Flags().BoolVar(&icoStrict, "strict", true, "Strict (keys must be in `--required` or `--optional`)")
+	//LATER: allowPng
+	//LATER: allowSvg
+}
+
+func uintContains(haystack []uint, needle uint) bool {
+	for _, straw := range haystack {
+		if straw == needle {
+			return true
+		}
+	}
+	return false
 }
 
 func icoCheck(f *shared.FileContext) {
@@ -33,7 +56,7 @@ func icoCheck(f *shared.FileContext) {
 		return
 	}
 
-	_, parseErr := ico.DecodeAll(bytes.NewReader(data))
+	images, parseErr := ico.DecodeAll(bytes.NewReader(data))
 
 	if parseErr != nil {
 		f.RecordResult("icoParse", false, map[string]interface{}{
@@ -41,4 +64,65 @@ func icoCheck(f *shared.FileContext) {
 		})
 		return
 	}
+
+	sizes := []uint{}
+
+	for index, image := range images {
+		b := image.Bounds()
+		width := b.Max.X - b.Min.X
+		height := b.Max.Y - b.Min.Y
+		if width < 0 || height < 0 {
+			f.RecordResult("icoSize", false, map[string]interface{}{
+				"bounds": b,
+				"error":  "invalid image size",
+				"height": height,
+				"index":  index,
+				"width":  width,
+			})
+			continue
+		}
+		if width != height {
+			f.RecordResult("icoSquare", false, map[string]interface{}{
+				"error":  "rectangular image",
+				"height": height,
+				"index":  index,
+				"width":  width,
+			})
+			continue
+		}
+		sizes = append(sizes, uint(width))
+	}
+	if shared.Debug {
+		fmt.Fprintf(os.Stderr, "DEBUG: icon sizes: %v\n", sizes)
+	}
+
+	if len(icoRequired) > 0 {
+		for _, key := range icoRequired {
+			f.RecordResult("icoRequired", uintContains(sizes, key), map[string]interface{}{
+				"key": key,
+			})
+		}
+	}
+
+	if icoStrict {
+		for _, key := range sizes {
+			_, ok := icoStrictSet[key]
+			f.RecordResult("icoStrict", ok, map[string]interface{}{
+				"size": key,
+			})
+		}
+	}
+}
+
+func icoPrepare(cmd *cobra.Command, args []string) error {
+	if icoStrict {
+		icoStrictSet = make(map[uint]bool)
+		for _, key := range icoRequired {
+			icoStrictSet[key] = true
+		}
+		for _, key := range icoOptional {
+			icoStrictSet[key] = true
+		}
+	}
+	return nil
 }
