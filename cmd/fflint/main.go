71397bfd0429723c8790926d91f081d7fc778464
Andrew Marcuse <fileformat@gmail.com>
2026-03-10 10:29:36 -0400

    Tweaks for homebrew

--- cmd/fflint/main.go
+++ cmd/fflint/main.go
@@ -10,10 +10,10 @@ import (
 )
 
 var (
-	version = "0.0.0"
-	commit  = "local"
-	date    = "local"
-	builtBy = "unknown"
+	VERSION = "0.0.0"
+	COMMIT  = "local"
+	LASTMOD = "local"
+	BUILTBY = "unknown"
 )
 
 func main() {
@@ -21,7 +21,7 @@ func main() {
 		Use:           "fflint",
 		Short:         "A linter to make sure your files are valid",
 		Long:          `See [www.fflint.dev](https://www.fflint.dev/) for detailed instructions`,
-		Version:       version,
+		Version:       VERSION,
 		SilenceErrors: true,
 		SilenceUsage:  true,
 	}
@@ -30,7 +30,7 @@ func main() {
 	shared.AddCommon(rootCmd)
 
 	command.AddAllCommands(rootCmd)
-	command.AddVersionCommand(rootCmd, command.VersionInfo{Commit: commit, Version: version, LastMod: date, Builder: builtBy})
+	command.AddVersionCommand(rootCmd, command.VersionInfo{Commit: COMMIT, Version: VERSION, LastMod: LASTMOD, Builder: BUILTBY})
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err.Error())
